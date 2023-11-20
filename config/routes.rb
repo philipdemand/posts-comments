539a3d321bacf9b3625bf47d5ac8ff58c530dc2e
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-11-20 11:50:45 -0500

    change name

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    resources :posts, only: [:create, :index, :update, :destroy]
-    resources :comments, only: [:create, :update, :destroy]
-    post '/posts/:id/comments', to: 'comments#create'
-  end
+
+  resources :posts, only: [:create, :index, :update, :destroy]
+  resources :comments, only: [:create, :update, :destroy]
+  post '/posts/:id/comments', to: 'comments#create'
+ 
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
