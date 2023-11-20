8313a271b11cc328403e70a54aa1594a6f61d381
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-11-20 11:42:21 -0500

    namespace and seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  
-  resources :posts, only: [:create, :index, :update, :destroy]
-  resources :comments, only: [:create, :update, :destroy]
-  post '/posts/:id/comments', to: 'comments#create'
+
+  namespace :api do
+    resources :posts, only: [:create, :index, :update, :destroy]
+    resources :comments, only: [:create, :update, :destroy]
+    post '/posts/:id/comments', to: 'comments#create'
+  end
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
