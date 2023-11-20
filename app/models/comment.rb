14d412fd302d8840cb8444ec19e3f1f56ad26771
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-11-20 17:02:40 -0500

    validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
     belongs_to :post
+    validates :comment_body presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
     has_many :comments
+    validates :post_body presence: true
 end
