7fb8f90a1941adf5adb92da5db93628d16d86bc2
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-12-06 23:33:16 -0500

    comments desc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-    has_many :comments, dependent: :destroy
+    has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
     validates :post_body, presence: true
 end
